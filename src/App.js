f079cf7c21a05343e348ad151ecc8163be211474
Aleh <user@iMac-User.fios-router.home>
2020-04-06 21:22:28 -0400

    component add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from './component/Box'
+
+const api = {
+  key:'4a3e8cc133747fa467515f1b8e7f9c75',
+  base: 'http://api.openweathermap.org/data/2.5/forecast?id=5128581&APPID=4a3e8cc133747fa467515f1b8e7f9c75',
+}
 
 function App() {
+
+  const builder = (e) => {
+    let days = ['Monday', 'Tuesday', ]
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box name='weatherBox'></Box>
+      <Box name='weatherBox'></Box>
+      <Box name='weatherBox'></Box>
+      <Box name='weatherBox'></Box>
     </div>
   );
 }
